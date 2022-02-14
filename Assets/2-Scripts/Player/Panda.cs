0b0b2d15720215736696524a534243c512a3b26a
youen <33334707+youenPlusquellec@users.noreply.github.com>
2022-02-14 01:40:16 +0100

    :memo: Add doc to panda

--- Assets/2-Scripts/Player/Panda.cs
+++ Assets/2-Scripts/Player/Panda.cs
@@ -8,7 +8,15 @@ namespace Karoushi
     /// Basic class of a Panda
     /// </summary>
     public class Panda : Player
-    {
+    {        
+        /// <summary>
+        /// Called when the player dies.
+        /// <example> Example(s) :
+        /// <code>
+        ///     player.Die();
+        /// </code>
+        /// </example>
+        /// </summary>
         protected override void Die()
         {
             Debug.Log("Oh no");
