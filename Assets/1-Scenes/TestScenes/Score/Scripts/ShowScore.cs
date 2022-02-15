26d083e71fa2b9d696aa034e6e4225a0663b11ae
Youen Plusquellec <youenetleo@gmail.com>
2022-02-15 18:21:10 +0100

    :truck: Move scenes scripts in a new namespace

--- Assets/1-Scenes/TestScenes/Score/Scripts/ShowScore.cs
+++ Assets/1-Scenes/TestScenes/Score/Scripts/ShowScore.cs
@@ -1,41 +1,43 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Karoushi;
 
-[RequireComponent(typeof(Text))]
-public class ShowScore : MonoBehaviour
+namespace Karoushi.Scene
 {
-    [SerializeField]
-    private Text text;
-
-    public void UpdateScore()
+    [RequireComponent(typeof(Text))]
+    public class ShowScore : MonoBehaviour
     {
-        string scoreText = "Total : " + ScoreManager.Instance.Score.TotalScore + "\n" +
-                         "Furnitures Score : " + ScoreManager.Instance.Score.FurnituresScore + "\n" +
-                         "Colleagues Score : " + ScoreManager.Instance.Score.ColleaguesScore + "\n" +
-                         "Casuals Score : " + ScoreManager.Instance.Score.CasualsScore + "\n";
+        [SerializeField]
+        private Text text;
 
-        scoreText += "\nTotal of Furnitures destroyed : " + ScoreManager.Instance.Score.DestroyedFurnituresCount + "\n";
-        foreach (KeyValuePair<string, int> entry in ScoreManager.Instance.Score.DestroyedFurnitures)
-        {   
-            scoreText += "\tNumber of " + entry.Key + " destroyed : " + entry.Value + "\n";
-        }
-
-        scoreText += "\nTotal of Colleagues destroyed : " + ScoreManager.Instance.Score.DestroyedColleaguesCount + "\n";
-        foreach (KeyValuePair<string, int> entry in ScoreManager.Instance.Score.DestroyedColleagues)
+        public void UpdateScore()
         {
-            scoreText += "\tNumber of " + entry.Key + " destroyed : " + entry.Value + "\n";
-        }
+            string scoreText = "Total : " + ScoreManager.Instance.Score.TotalScore + "\n" +
+                             "Furnitures Score : " + ScoreManager.Instance.Score.FurnituresScore + "\n" +
+                             "Colleagues Score : " + ScoreManager.Instance.Score.ColleaguesScore + "\n" +
+                             "Casuals Score : " + ScoreManager.Instance.Score.CasualsScore + "\n";
 
-        scoreText += "\nTotal of Furnitures destroyed : " + ScoreManager.Instance.Score.DestroyedCasualsCount + "\n";
-        foreach (KeyValuePair<string, int> entry in ScoreManager.Instance.Score.DestroyedCasuals)
-        {
-            scoreText += "\tNumber of " + entry.Key + " destroyed : " + entry.Value + "\n";
-        }
+            scoreText += "\nTotal of Furnitures destroyed : " + ScoreManager.Instance.Score.DestroyedFurnituresCount + "\n";
+            foreach (KeyValuePair<string, int> entry in ScoreManager.Instance.Score.DestroyedFurnitures)
+            {
+                scoreText += "\tNumber of " + entry.Key + " destroyed : " + entry.Value + "\n";
+            }
 
-        this.text.text = scoreText;
+            scoreText += "\nTotal of Colleagues destroyed : " + ScoreManager.Instance.Score.DestroyedColleaguesCount + "\n";
+            foreach (KeyValuePair<string, int> entry in ScoreManager.Instance.Score.DestroyedColleagues)
+            {
+                scoreText += "\tNumber of " + entry.Key + " destroyed : " + entry.Value + "\n";
+            }
+
+            scoreText += "\nTotal of Furnitures destroyed : " + ScoreManager.Instance.Score.DestroyedCasualsCount + "\n";
+            foreach (KeyValuePair<string, int> entry in ScoreManager.Instance.Score.DestroyedCasuals)
+            {
+                scoreText += "\tNumber of " + entry.Key + " destroyed : " + entry.Value + "\n";
+            }
+
+            this.text.text = scoreText;
+        }
     }
-
 }
